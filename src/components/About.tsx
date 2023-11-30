19f1d6fed2549922f65fc2fd9ee8d96a0900f30f
Greg Smelkov <gsmelkov@gmail.com>
2023-11-30 09:28:55 -0500

    fixed for mobile view

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,8 +8,8 @@ const About = () => {
         <div>Hi!</div>
         <div>
           I've been taking photos for a few years and I finally decided I needed
-          to show them somehow. These are some of the ones that I thought came
-          out pretty well.
+          to show them somehow. Here's a few that I thought came out pretty
+          well.
         </div>
         <div>I use a Fujifilm X-T3 with a 16-80mm and a 35mm prime lens.</div>
       </div>
