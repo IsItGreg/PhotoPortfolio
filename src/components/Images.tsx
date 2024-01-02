fea9ba95c104eed10f06a926cc0944daae9f8372
IsItGreg <gsmelkov@gmail.com>
2024-01-02 17:23:09 -0500

    added image zoom in/out

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,6 +1,13 @@
-import { photoCards, PhotoCard } from "../photos";
+import { useState } from "react";
+import { photoCards, PhotoCard, Photo } from "../photos";
 
-const PhotoCardPanel = ({ photoCard }: { photoCard: PhotoCard }) => {
+const PhotoCardPanel = ({
+  photoCard,
+  setFullscreen,
+}: {
+  photoCard: PhotoCard;
+  setFullscreen: (photo: Photo) => void;
+}) => {
   return (
     <section className="flex h-full snap-center flex-col items-center justify-center px-5 py-5 sm:px-24 sm:py-10">
       <div className="aspect-17/20 flex h-full max-w-full flex-col content-stretch justify-center gap-3">
@@ -16,9 +23,10 @@ const PhotoCardPanel = ({ photoCard }: { photoCard: PhotoCard }) => {
                       }
                     >
                       <img
-                        className="h-full w-full object-cover shadow-lg"
+                        className="h-full w-full cursor-zoom-in object-cover shadow-lg transition ease-in-out hover:scale-[1.02]"
                         src={photo.src}
                         alt={photo.title}
+                        onClick={() => setFullscreen(photo)}
                       />
                     </div>
                   );
@@ -33,10 +41,37 @@ const PhotoCardPanel = ({ photoCard }: { photoCard: PhotoCard }) => {
 };
 
 export const Images = () => {
+  const [fullscreenedImage, setFullscreenedImage] = useState<Photo | null>(
+    null,
+  );
+
   return (
     <div className="h-full snap-y snap-mandatory overflow-y-auto">
       {photoCards.map((photoCard, index) => {
-        return <PhotoCardPanel key={index} photoCard={photoCard} />;
+        return (
+          <>
+            <div
+              className={`absolute left-0 top-0 z-10 flex h-screen w-screen cursor-zoom-out items-center justify-center bg-black bg-opacity-30 backdrop-blur-sm transition duration-100 ease-in-out ${
+                fullscreenedImage ? "opacity-100" : "hidden opacity-0"
+              }`}
+              onClick={() => {
+                setFullscreenedImage(null);
+              }}
+            >
+              <img
+                className="max-h-[95%] max-w-[95%] object-cover md:max-h-[90%] md:max-w-[90%]"
+                src={fullscreenedImage?.src}
+              />
+            </div>
+            <PhotoCardPanel
+              key={index}
+              photoCard={photoCard}
+              setFullscreen={(photo: Photo | null) => {
+                setFullscreenedImage(photo);
+              }}
+            />
+          </>
+        );
       })}
     </div>
   );
